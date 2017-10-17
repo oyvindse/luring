5a1a66d85cbc7e2693dcbdb10a1ab449d034a4e8
Øyvind Selmer <oyvind.selmer@gmail.com>
2017-10-18 00:38:14 +0200

    updated storage stuff

--- index.js
+++ index.js
@@ -5,20 +5,27 @@ var phantomjs = require('phantomjs-prebuilt');
 var yr = require('yr.no-forecast');
 var adage = require('adage');
 var query = require('game-server-query');
+var path = require('path');
+var fs = require('fs');
 
 var busometer = require('./busometer.js');
 
 var config = {
 	channels: ["#testing123"],
 	server: "irc.efnet.no",
-	clientName: "noNameBot"
+	clientName: "noNameLuring"
 };
 
 var client = new irc.Client(config.server, config.clientName, {
 	channels: config.channels
 });
 
+var storage = path.join(__dirname, 'storedProps.json');
+
 var listeners = [];
+var ignored = [];
+
+setInterval(persist, 10000); // timed storage: temp every 10sec
 
 client.addListener('message', function (from, to, message) {
     if("T-bane?".toUpperCase() === message.toUpperCase()) {
@@ -32,6 +39,8 @@ client.addListener('message', function (from, to, message) {
       luring(message);
     }
 });
+
+
 
 function on(trigger, callback) {
 	var clientListener = (f,t,m) => {
@@ -57,14 +66,120 @@ function off(trigger) {
 	});
 }
 
+function traverse(o, cb) {
+  if (typeof o === 'undefined' || o === null) return '';
+  if (Array.isArray(o)) {
+    return o.map(function(e, i) {
+      return traverse(e, cb);
+    });
+  } else if (typeof o === 'object') {
+    console.log(o);
+    if (('' + o).match(/^\/.*\/$/)) {
+      return '' + o;
+    }
+
+    return Object.keys(o).reduce(function(o2, key) {
+      o2[key] = traverse(o[key], cb);
+      return o2;
+    }, {});
+  } else {
+    return cb(o);
+  }
+}
+
 function say(message) {
 	client.say(config.channels[0], message);
 }
 
+//persist
+//function keys() {
+//return Object.keys(this).filter(function(key) {
+ //   return ignored.indexOf(key) < 0;
+ // });
+//}
+
+function toJSON(o) {
+  o = traverse(o, function(o) {
+    if (typeof o === 'function') {
+      return '' + o;
+    }
+    return o;
+  });
+  return JSON.stringify(o);
+}
+
+function fromJSON(json) {
+  var data = traverse(JSON.parse(json), function(o) {
+    if (typeof o === 'string') {
+      try {
+        if (o.match(/^function|^\(\w*\)=>/)) {
+          return eval('evalhack=' + o);
+        } else if (o.match(/^\/.*\/$/)) {
+          return eval(o);
+        }
+      } catch (e) {}
+    }
+    return o;
+  });
+  delete evalhack;
+  return data;
+}
+
+function keys() {
+  return Object.keys(this).filter(function(key) {
+    return ignored.indexOf(key) < 0;
+  });
+}
+
+
+setTimeout(function() {
+  (function() {
+    ignored = Object.keys(this);
+
+    if (typeof this.listeners === 'undefined') {
+      this.listeners = [];
+    }
+    if (typeof this.onload === 'undefined') {
+      this.onload = [];
+    }
+
+    fs.readFile(storage, function(err, data) {
+      var evaled;
+      if (!err) {
+        try {
+          evaled = fromJSON(data);
+          Object.keys(evaled).forEach(function(key) {
+            this[key] = evaled[key];
+          });
+        } catch (e) {}
+      }
+      if (Array.isArray(this.listeners)) {
+        this.listeners.forEach(function(listener) {
+          addListener(listener);
+        });
+      }
+    });
+  }());
+}, 1)
+
+function persist() {
+  var evaled = keys().reduce(function(o, key) {
+    o[key] = this[key];
+    return o;
+  }, {});
+  fs.writeFile(storage, toJSON(evaled));
+  console.log('Wrote to file');
+  console.log(evaled);
+}
+
+
+//!persist
 function luring(message) {
 	try {
 		var e = eval(message);
 	} catch (err) {
-		console.error(err);
+    console.error(err);
 	}
 }
+
+
